8eb820db59a95a6b0a4fc7e1350eaf5bf2e9a618
beva-empa <75076601+beva-empa@users.noreply.github.com>
2021-02-03 15:41:20 +0100

    new commit to multi

--- comp_classes.py
+++ comp_classes.py
@@ -129,87 +129,18 @@ class Elec_Grid:
         self.b_GridIn = cp.Variable(num_opt_var, boolean=True)
         self.b_GridOut = cp.Variable(num_opt_var, boolean=True)
 
-# num_opt_var = 24
-# cost = 0
-# constr = []
-# I_Solar = cp.Variable(num_opt_var)
-# Pmax_PV = 0
-# d_PV = 1
-# P_Comp = cp.Variable(num_opt_var)
-# Eff_PV = 1
-# TempEff_PV = 1
-# d = PV(I_Solar, num_opt_var, Pmax_PV, d_PV)
-# print(d.I_PV)
-#
-# beta = 0.003
-# d_PV = 0.15
-# d_PVT = 0.18
-# COP = 4.5
-# C_Fuel = 0.115
-# Tstc = 25
-# Tnoct = 48.3
-# Tstd = 20
-# time_start = dt.datetime(2018, 1, 1, 0, 0, 0)
-# time_end = dt.datetime(2018, 1, 1, 3, 0, 0)
-# time_now = time_start
-# start_time = dt.datetime(2018, 1, 1)
-# num_opt_var = 24
-# constr = []
-# Pmax_PV = 2540
-# Eff_PV = 0.3
-# Pmin_PV = 0
-# demand_data = get_data(start_time)
-# Ta = demand_data.loc[time_now: time_now + dt.timedelta(hours=23), 'temp'].values.tolist()
-# I_Solar = demand_data.loc[time_now: time_now + dt.timedelta(hours=23), 'solar_roof'].values.tolist()
-# P_Demand = demand_data.loc[time_now: time_now + dt.timedelta(hours=23), 'elec_1'].values.tolist()
-# li = []
-# li.append(PV(num_opt_var))
-#
-#
-# print(li[0].P_PV)
-#
-# for t in range(num_opt_var):
-#     li[0].I_PV[t] = I_Solar[t] * (Pmax_PV / d_PV)
-#     li[0].TempEff_PV[t] = 1 + ((-beta) * ((Ta[t] - Tstc) + (Tnoct - Ta[t]) * (I_Solar[t] / 0.8)))
-#
-# for t in range(num_opt_var):
-#
-#     constr += [li[0].P_PV[t] >= 0,
-#                li[0].P_PV[t] <= li[0].b_PV[t] * Eff_PV * li[0].TempEff_PV[t] * li[0].I_PV[t]]
-#
-# print('ghb')
-#
-# C_Grid = cp.Variable(num_opt_var)
-# P_GridIn = cp.Variable(num_opt_var)
-# P_GridOut = cp.Variable(num_opt_var)
-#
-# R_GridOut = demand_data.loc[time_now: time_now + dt.timedelta(hours=23), 'el_tariff'].values.tolist()
-# R_GridIn = demand_data.loc[time_now: time_now + dt.timedelta(hours=23), 'feed_in_tariff'].values.tolist()
-#
-# b_GridIn = cp.Variable(num_opt_var, boolean=True)
-# b_GridOut = cp.Variable(num_opt_var, boolean=True)
-#
-# cost = 0
-# for t in range(num_opt_var):
-#     # Demand
-#     cost += C_Grid[t]
-#     constr += [P_Demand[t] == (li[0].P_PV[t] + P_GridOut[t] - P_GridIn[t]),
-#                b_GridIn[t] + b_GridOut[t] <= 1,
-#                P_GridIn[t] >= 0, P_GridIn[t] <= 10000000000000 * b_GridIn[t],
-#                P_GridOut[t] >= 0, P_GridOut[t] <= 10000000000000 * b_GridOut[t],
-#                C_Grid[t] == R_GridOut[t] * P_GridOut[t] - R_GridIn[t] * P_GridIn[t]]
-#
-#     # Solve with mosek or Gurobi
-# problem = cp.Problem(cp.Minimize(cost), constr)
-# problem.solve(solver=cp.MOSEK, verbose=True, save_file='opt_diagnosis.opf',
-#               mosek_params={mosek.iparam.intpnt_solve_form: mosek.solveform.dual,
-#                             mosek.dparam.optimizer_max_time: 100.0})
-# print('test')
-# class test1:
-#     def __init__(self, cost1):
-#         self.cost1 = cost1 + 5
-#
-# cost = 5
-# d = test1(cost)
-# cost = d.cost1
-# print(cost)
+
+class trans_pw_ht:
+    def __init__(self, num_opt_var, st, rt):
+
+        self.P_strt = cp.Variable(num_opt_var)
+        self.Q_strt = cp.Variable(num_opt_var)
+        self.origin = st
+        self.recpnt = rt
+
+class hub_net:
+    def __init__(self, num_opt_var):
+
+        self.b_hubIn = cp.Variable(num_opt_var, boolean=True)
+        self.b_hubOut = cp.Variable(num_opt_var, boolean=True)
+
